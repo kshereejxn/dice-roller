495209fc781c338fff04b2949d44d529176f3fd1
Trinie J <jxsn00ks@gmail.com>
2020-05-18 07:06:17 -0400

    rolling dice working

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-let numberdice = document.querySelector ('#numberdice');
-let sumtotal = document.querySelector (".sumtotal");
-let indtotal = document.querySelector (".indtotal");
-let rollingdice = document.querySelector ("rollingdice");
+let showAllRollsButton = document.querySelector ("#show-all-rolls-button");
+let roll = document.querySelector ("#roll");
+let numberOfRolls =document.querySelector ("#number-of-rolls");
+let total = document.querySelector ("#total");
+var dieRolls= [];
+dieRolls.push(1, 2, 3, 4, 5, 6);
 
+
+
+
+roll.addEventListener("click", function()
+{
+console.log ("rolling dice")
+
+})
+
+/*var dice = Math.floor(Math.random() *6) =1;
+roll.innerHTML = dice;
+total.innerHTML = " +dice+";
+})*/
+
+/*let newDiceString = '<li class="dice">' = numberRolled + "</li>"; allRollsElement.innerHTML += newDiceString;*/
+
+/*let list-for-rolls = document.querySelector ('#listForRolls');*/
+
+/*total.innerHtml*/
+
+/*var dieRolls = []; document.getElementById("total").innerHTML = dieRolls;*/
